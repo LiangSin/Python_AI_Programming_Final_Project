adb3a9bc7d016ca9200d729545f9f21409ea72a4
賴亮昕 <sunny990284@gmail.com>
2024-11-17 15:07:32 +0800

    modify training batch size

--- YOLO_tool/train.py
+++ YOLO_tool/train.py
@@ -1,14 +1,14 @@
 import argparse
 from ultralytics import YOLO
 
-def train(model_path, yaml_path, device):
+def train(model_path, yaml_path, device, batch):
     # Load a model
     model = YOLO(model_path)  # build a new model from scratch
 
     # Use the model
     if model_path.endswith('.yaml'):
         results = model.train(data=yaml_path, imgsz=256, device=device, verbose=True,
-                            epochs=500, patience=50, 
+                            epochs=500, patience=50, batch=batch,
                             weight_decay=0.001, dropout=0.3)
     elif model_path.endswith('.pt'):
         results = model.train(resume=True)
@@ -19,7 +19,8 @@ if __name__ == "__main__":
     parser.add_argument("--model", type=str, default="yolov8n.yaml", help="The default is training from scratch. Input the existing .pt model file to continue the training process.")
     parser.add_argument("--config", type=str, default="coco.yaml", help="Path to the config file")
     parser.add_argument("--device", type=str, default=0, help="device number")
+    parser.add_argument("--batch", type=int, default=-1, help="Batch size for training. Default is alto mode for 60% GPU utilization")
     args = parser.parse_args()
 
     # Run the main function
-    train(args.model, args.config, args.device)
+    train(args.model, args.config, args.device, args.batch)
